b6447e29baeb1657e30cab96c961fc6513a3ed71
yuanleyou <yuanshengrong0413@foxmail.com>
2022-10-20 09:04:08 -0700

    libbpf hello

--- eBPF-C/run.sh
+++ eBPF-C/run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# docker build -t ubuntu20.04-ebpf-C:v1 .  # 构建测试环境基础镜像，第一次的时候运行
+# docker build -t ubuntu20.04-ebpf-c:v1 .  # 构建测试环境基础镜像，第一次的时候运行
 
 # shellcheck disable=SC2046
 # shellcheck disable=SC2164
-workdir=$(cd $(dirname "$0"); pwd)
+workdir=$(dirname $(pwd))
 
-mount -t debugfs debugfs /sys/kernel/debug
+# mount -t debugfs debugfs /sys/kernel/debug
 
 docker run -it --rm \
 	--privileged \
+	-v /sys/kernel/debug:/sys/kernel/debug:ro \
 	--mount type=bind,source="$workdir",destination=/home/ebpf \
-	--workdir /home/ebpf \
-	ubuntu20.04-ebpf-C:v1 bash
+	--workdir /home/ebpf/eBPF-C \
+	ubuntu20.04-ebpf-c:v1 bash
